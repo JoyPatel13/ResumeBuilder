5c3eb7d6b56857a1bef852b65b5397c180ccfe8e
Patel Joy Rajeshkumar <226872928+JoyPatel13@users.noreply.github.com>
2026-06-04 22:51:16 +0530

    Update Register.jsx

--- frontend/src/features/auth/pages/Register.jsx
+++ frontend/src/features/auth/pages/Register.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import {useNavigate , Link} from 'react-router'
+
 const Register=()=>{
+
+    const navigate = useNavigate();
+
     const handleSubmit=(e)=>{
         e.preventDefault()
     }
@@ -8,6 +13,11 @@ const Register=()=>{
             <div className="form-container">
                 <h1>Register</h1>
                 <form onSubmit={handleSubmit}>
+                   <div className="input-group">
+                        <label htmlFor="username">Username</label>
+                        <input type="text" id="username" name="username"  placeholder="Enter username"/>
+                    
+                    </div> 
                    <div className="input-group">
                         <label htmlFor="email">Email</label>
                         <input type="email" id="email" name="email"  placeholder="Enter Email"/>
@@ -19,6 +29,7 @@ const Register=()=>{
                     </div>
                     <button className="button primary-button">Register</button>
                 </form>
+                <p>Already have an account ? <Link to={'/login'}>Login</Link> </p>
             </div>
         </main>
     )
