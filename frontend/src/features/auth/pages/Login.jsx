d8d141f56a7d45e9d74e26cccd2a04ea77ac5024
Patel Joy Rajeshkumar <226872928+JoyPatel13@users.noreply.github.com>
2026-06-04 16:31:40 +0530

    Add login form and styles

--- frontend/src/features/auth/pages/Login.jsx
+++ frontend/src/features/auth/pages/Login.jsx
@@ -1,8 +1,25 @@
 import React from "react";
+import "../auth.form.scss"
 
 const Login = ()=>{
     return(
-        <div>Login</div>
+        <main>
+            <div className="form-container">
+                <h1>Login</h1>
+                <form>
+                   <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" name="email"  placeholder="Enter Email"/>
+                    
+                    </div> 
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" id="password" />
+                    </div>
+                    <button className="button primary-button">Login</button>
+                </form>
+            </div>
+        </main>
     )
 }
 
