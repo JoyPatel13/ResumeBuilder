a034fdd1ef79759c8164e2bb23ee31499b6150be
Patel Joy Rajeshkumar <226872928+JoyPatel13@users.noreply.github.com>
2026-06-04 22:47:40 +0530

    Update Register.jsx

--- frontend/src/features/auth/pages/Register.jsx
+++ frontend/src/features/auth/pages/Register.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 const Register=()=>{
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+    }
     return(
-        <div>Register</div>
+        <main>
+            <div className="form-container">
+                <h1>Register</h1>
+                <form onSubmit={handleSubmit}>
+                   <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" name="email"  placeholder="Enter Email"/>
+                    
+                    </div> 
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" id="password" />
+                    </div>
+                    <button className="button primary-button">Register</button>
+                </form>
+            </div>
+        </main>
     )
 }
 
