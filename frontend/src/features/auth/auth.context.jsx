243880401370d566bd63e229739b123d2d29f393
Patel Joy Rajeshkumar <226872928+JoyPatel13@users.noreply.github.com>
2026-06-05 17:23:11 +0530

    Update auth.context.jsx

--- frontend/src/features/auth/auth.context.jsx
+++ frontend/src/features/auth/auth.context.jsx
@@ -6,7 +6,7 @@ export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading,setLoading] = useState(false);
    return(
-    <AuthContext.Provider value={user,setUser,loading,setLoading}>
+    <AuthContext.Provider value={{user,setUser,loading,setLoading}}>
         {children}
     </AuthContext.Provider>
    )
