e3c1873f66b70c4ab676a40f5e1e9c5af39eb670
Patel Joy Rajeshkumar <226872928+JoyPatel13@users.noreply.github.com>
2026-06-06 15:55:28 +0530

    Create ai.service.js

---
+++ backend/src/services/ai.service.js
@@ -0,0 +1,12 @@
+const {GoogleGenAI} = require("@google/genai")
+
+const ai = new GoogleGenAI({
+    apiKey : process.env.GOOGLE_GENAI_API_KEY
+});
+
+
+async function invokeGeminiAi(){
+    const response = await ai.models.generateContent({
+       model : "gemini-2.0-flash", 
+    })
+}
