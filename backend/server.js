60d6c05da4341d4edff9657f722902221a9f1d49
Patel Joy Rajeshkumar <226872928+JoyPatel13@users.noreply.github.com>
2026-06-06 18:21:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,9 @@
 require("dotenv").config();
 const app = require('./src/app');
 const connectToDb = require("./src/config/database");
-const {resume , selfDescription , jobDescription} = require("./src/services/temp")
-const genereateInterviewReport = require("./src/services/ai.service")
 
 connectToDb();
 
-genereateInterviewReport({resume , selfDescription , jobDescription})
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
