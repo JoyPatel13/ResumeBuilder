27ba3c860be7aea9e48d132c512109f08f1e94a3
Patel Joy Rajeshkumar <226872928+JoyPatel13@users.noreply.github.com>
2026-06-06 21:56:38 +0530

    Add interview route and empty controller
    Create a new empty interview controller module and update interview routes to add a POST /api/interview endpoint. The route includes an auth middleware and references the interview controller, and includes a JSDoc comment describing the endpoint (generate interview report from user input, resume PDF, and job description).

--- backend/src/routes/interview.routes.js
+++ backend/src/routes/interview.routes.js
@@ -1,6 +1,17 @@
 const express = requires('express');
+const authMiddlewar = require('../middlewares/auth.middleware')
+const interviewController = require('../controllers/interview.controller')
 
 const interviewRouter = express.Router();
+
+/**
+ * @route POST /api/interview
+ * @description generate new interview report on the basis of user self description
+ * , resume pdf and job description 
+ * @access Private
+ */
+
+interviewRouter.post('/' , authMiddlewar.authUser , interviewController.generateInterviewReportController);
 
 
 module.exports = interviewRouter
