2e8874c031ec64780e3a836645e8f2fef251c412
Patel Joy Rajeshkumar <226872928+JoyPatel13@users.noreply.github.com>
2026-06-07 22:22:55 +0530

    Update interview.api.js

--- frontend/src/features/interview/services/interview.api.js
+++ frontend/src/features/interview/services/interview.api.js
@@ -1,5 +1,33 @@
 import axios from "axios";
+
 const api = axios.create({
     baseURL: "http://localhost:3000",
     withCredentials:true
 })
+
+export const generateInterviewReport = ({jobDescription , selfDescription , resumeFile})=>{
+    const formData = new formData()
+    formData.append("jobDescription" , jobDescription)
+    formData.append("selfDescription" , selfDescription)
+    formData.append("resume" , resumeFile);
+    const response =  api.post("/api/interview/" , formData,{
+        headers:{
+            "Content-Type" : 'multipart/form-data'
+        }
+    })
+    return response.data
+
+}
+
+
+
+export const getInterviewReportById = (interviewId)=>{
+    const response = api.get(`/api/interview/report/${interviewId}`)
+    return reponse.data;
+}
+
+
+export const getAllInterview = ()=>{
+    const response = api.get('/api/interview');
+    return response.data
+}
