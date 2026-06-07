be6064c44d06f05c7fceba91d6909c0ec91667ca
Patel Joy Rajeshkumar <226872928+JoyPatel13@users.noreply.github.com>
2026-06-07 11:41:21 +0530

    Create Home.jsx

---
+++ frontend/src/features/interview/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <main className="home">
+        <div className="left">
+            <textarea name="jobDescription"  id="jobDescription" placeholder='Enter job description here...'></textarea>
+        </div>
+        <div className="right">
+            <div className="input-group">
+                <label htmlFor="resume">Upload resume</label>
+                <input type="file" name="resume" id="resume" accept='.pdf' />
+            </div>
+            <div className="input-group">
+                <label htmlFor="selfDescription">Self Description</label>
+                <textarea  name="selfDescription" id="selfDescription" placeholder='Describe yourself in a few sentences...' ></textarea>
+            </div>
+            <button className="generate-btn">Generate Interview Report</button>
+        </div>
+    </main>
+  )
+}
