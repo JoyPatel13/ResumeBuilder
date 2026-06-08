c6c1158ac66083d12eb90d0ba3f71a3bfb68dc02
Patel Joy Rajeshkumar <226872928+JoyPatel13@users.noreply.github.com>
2026-06-08 12:40:08 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,8 +7,12 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin: process.env.CLIENT_URL || 'http://localhost:5173',
-    credentials:true
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }))
+app.options('*', cors())
+
 
 //require all the routes here
 const authRouter = require('./routes/auth.routes');
